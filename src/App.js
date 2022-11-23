e1ea71a67e1d4517bcf729ccb8af54a97b349f68
Luke Passey <ljpassey@gmail.com>
2022-11-22 18:31:59 -0700

    This commit includes the day 3 updates to my Unit 1 React project.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css'
+import React, { useState } from "react";
+
+import "./App.css";
 import Square from "./components/Square.js";
 
-
 function App() {
-  const propVariable = "This is a prop"
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
   return (
     <div className="App">
-      <Square propVar={propVariable} />
+      <Square
+        squares={squares}
+        setSquares={setSquares}
+        player={player}
+        setPlayer={setPlayer}
+      />
     </div>
   );
 }

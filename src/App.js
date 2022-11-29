bf8051cf6cdd3b23f536ebeaf1f597d236b48d1b
Luke Passey <ljpassey@gmail.com>
2022-11-28 17:42:51 -0700

    This commit includes the completed Unit-1 Project with everything completed and no errors.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,32 @@ function App() {
 
   const handleClick = () => {
     setSquares(["", "", "", "", "", "", "", "", ""]);
-    setPlayer(true)
-  }
+    setPlayer(true);
+  };
 
-  
+  const calculateWinner = (arr) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} won!`;
+      }
+    }
+    return "Who will win?";
+  };
 
   return (
     <div className="App">
+      <span>{calculateWinner(squares)}</span>
       <div className="container">
         {squares.map((val, index) => {
           return (
